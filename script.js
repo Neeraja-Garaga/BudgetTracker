5c7ccd9d07e1b7defc2c863b95faef1dca15f58c
Neeraja-Garaga <garaganiraja2005@gmail.com>
2025-03-22 00:03:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ function updateValues() {
   const income = amounts.filter(item => item > 0).reduce((acc, item) => acc + item, 0).toFixed(2);
   const expense = (amounts.filter(item => item < 0).reduce((acc, item) => acc + item, 0) * -1).toFixed(2);
 
-  balance.innerText = $${total};
-  incomeDisplay.innerText = $${income};
-  expenseDisplay.innerText = $${expense};
+  balance.innerText = `$${total}`;
+  incomeDisplay.innerText = `$${income}`;
+  expenseDisplay.innerText = `$${expense}`;
 }
 
 // Remove Transaction
